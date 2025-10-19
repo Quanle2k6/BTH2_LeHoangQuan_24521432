9e318f52724f992bea5f428bca5dc8a74abfbf0f
123456 <quanle2006295@gmail.com>
2025-10-20 00:42:47 +0700

    Add Bai02.cs

--- BHT2_LeHoangQuan_24521432/Program.cs
+++ BHT2_LeHoangQuan_24521432/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq.Expressions;
 using Bai01;
+using Bai02;
 
 namespace Program
 {
@@ -7,10 +9,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bai 1:");
-            B1 date = new B1();
-            date.Run();
+            //Console.WriteLine("Bai 1:");
+            //B1 b1 = new B1();
+            //b1.Run();
+            //Console.WriteLine("----------------------");
+            Console.WriteLine("Bai 2:");
+            B2 b2 = new B2();
+            b2.Run();
             Console.WriteLine("----------------------");
+
         }
     }
 }
