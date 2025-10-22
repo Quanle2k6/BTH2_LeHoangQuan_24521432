c22d6cf77981e3c9051a3e880d646eed0b118b00
123456 <quanle2006295@gmail.com>
2025-10-22 13:44:11 +0700

    change1

---
+++ BHT2_LeHoangQuan_24521432/Bai05.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHT2_LeHoangQuan_24521432
+{
+    internal class Bai05
+    {
+    }
+}
